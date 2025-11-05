91c63cafa3e91977e8aa4109b57aa81b3635837a
Darwin Bicknell <ineedacure@gmail.com>
2025-11-05 20:32:39 +0000

    Property 'card' does not exist on type 'JSX.IntrinsicElements'.

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -129,7 +130,7 @@ export default function PublicProfilePage({ params }: PublicProfilePageProps) {
                         </Button>
                     )}
                 </CardContent>
-            </card>
+            </Card>
         </div>
     );
 }
