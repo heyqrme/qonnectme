dfba934e52d949526508af96489c991d2a283b09
Darwin Bicknell <ineedacure@gmail.com>
2025-11-09 20:12:09 +0000

    auth/api key not valid

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: './.env.local' });
+
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
