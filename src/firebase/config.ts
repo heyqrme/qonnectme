40e0684cba1cd24becdcdce9795558e95a72299d
Darwin Bicknell <ineedacure@gmail.com>
2025-11-09 23:30:57 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "qonnectme",
   "appId": "1:727140053765:web:af31f026d541405fae7d6c",
+  "storageBucket": "qonnectme.appspot.com",
   "apiKey": "AIzaSyADnNYvTcDHjlMqPZVslp35WM5P1e83NNM",
   "authDomain": "qonnectme.firebaseapp.com",
   "measurementId": "",
