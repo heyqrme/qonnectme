3c3e8ea205853220bd91502bee40e507a996bf45
Darwin Bicknell <ineedacure@gmail.com>
2025-11-10 02:25:41 +0000

    Trigger new deployment after fixing IAM permissions

--- next.config.js
+++ next.config.js
@@ -1,26 +1,25 @@
-
-
- /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Adding a comment to trigger a new deployment.
   images: {
     remotePatterns: [
       {
-        protocol: 'https'',
-        hostname: 'placehold.co''',
-        port: '''',
-        pathname: '/**'',
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: 'https'',
-        hostname: 'api.qrserver.com''',
-        port: '''',
-        pathname: '/**'',
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: 'https'',
-        hostname: 'i.imgur.com''',
-        port: '''',
-        pathname: '/**'',
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
